823af8b584d1ef7598957944686482b18b0f80ff
AlexanderL <al.x.300000@gmail.com>
2021-03-06 12:50:54 -0500

    created window

--- src/Window.java
+++ src/Window.java
@@ -1,4 +1,22 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class Window {
-
+	Window(int width, int height){
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		JFrame frame = new JFrame();
+		
+		frame.pack();
+		
+		frame.setVisible(true);
+		frame.setPreferredSize(new Dimension(width, height));
+		frame.setMaximumSize(new Dimension(width, height));
+		frame.setMinimumSize(new Dimension(width, height));
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.setResizable(true);
+		
+	}
+	
 }
