4cda4ee8d4487e62bc32f7a4e0bc3b9a6d82a9a6
AlexanderL <al.x.300000@gmail.com>
2021-03-10 17:12:26 -0500

    Created a new thread and sorted some things

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,25 @@
+import java.awt.Canvas;
 
-public class Main implements Runnable{
-
+public class Main extends Canvas implements Runnable{
+	
+	
+	private boolean running = false;
+	private Thread thread;
+	
+	
 	public static void main(String[] args) {
-		new Window(1000,1000);
+		new Window(1000,1000, new Main());
 	}
-
-	public void run() {
+	
+	public synchronized void start() {
+		if(running) return;
 		
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	public void run() {
+		System.out.println("Test");
 		
 	}
 	
--- src/Window.java
+++ src/Window.java
@@ -3,19 +3,23 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 
 public class Window {
-	Window(int width, int height){
+	Window(int width, int height, Main main){
 		JFrame.setDefaultLookAndFeelDecorated(true);
+
+		
+		main.setPreferredSize(new Dimension(width, height));
+		main.setMaximumSize(new Dimension(width, height));
+		main.setMinimumSize(new Dimension(width, height));
+		
 		JFrame frame = new JFrame();
-		
+		frame.add(main);
 		frame.pack();
-		
-		frame.setVisible(true);
-		frame.setPreferredSize(new Dimension(width, height));
-		frame.setMaximumSize(new Dimension(width, height));
-		frame.setMinimumSize(new Dimension(width, height));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-		frame.setResizable(true);
+		frame.setVisible(true);
+		
+		main.start();
 		
 	}
 	
